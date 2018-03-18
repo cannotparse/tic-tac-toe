b27744234c2b51caeaa083d4e2f3070a96afaccb
Rodger <rwald@uocsclub.ca>
2018-03-18 16:00:22 -0400

    Fixing travis?

---
+++ tictactoeclient/src/test/java/ca/uocsclub/bot/TestTemp.java
@@ -0,0 +1,12 @@
+package ca.uocsclub.bot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestTemp {
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
