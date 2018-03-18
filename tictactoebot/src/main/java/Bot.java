3cd12485f12e39aa4b1b9ba67fa2a553e798a695
Rodger <rwald@uocsclub.ca>
2018-03-18 13:45:24 -0400

    Created modules with individual hello world

---
+++ tictactoebot/src/main/java/Bot.java
@@ -0,0 +1,5 @@
+public class Bot {
+    public void main(String[] args){
+        System.out.println("Hello world from client");
+    }
+}
---
+++ tictactoeserver/src/main/java/Server.java
@@ -0,0 +1,5 @@
+public class Server {
+    public void main(String [] args){
+        System.out.println("Hello World Server");
+    }
+}
